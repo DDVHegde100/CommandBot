a877fcc117869049949dabb537a996040d2b5c3a
DDVHegde100 <69629876+DDVHegde100@users.noreply.github.com>
2021-05-27 19:09:43 -0400

    Update command.js

--- command.js
+++ command.js
@@ -2,7 +2,7 @@ const Discord=require('discord.js');
 const client=new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 
 const prefix= '!';
-
+//Unfinished Code for Bot.
 client.once('ready', ()=>{ 
     console.log('Ready to be Big Brain'); 
 })
